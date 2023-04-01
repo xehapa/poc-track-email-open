0889399c54fd63f19e9a63885a9c79d0c09c9bdc
Xehapa <xehapa.dev@gmail.com>
2023-04-01 11:04:12 +0700

    update .env

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function __invoke(): BinaryFileResponse
+    public function __invoke(): Response
     {
+        return new Response($this->getParameter('MAILER_DSN'));
         return $this->file('stockclubs.webp', 'stockclubs-logo.webp', ResponseHeaderBag::DISPOSITION_INLINE);
     }
 }
